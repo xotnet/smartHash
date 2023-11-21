16de877447228c2c7cc9b0d4bc4fa4e39a68b579
xotnet <150250186+xotnet@users.noreply.github.com>
2023-11-21 10:39:51 +0000

    Create smartHash.cpp

---
+++ smartHash.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring>
+const int hashLenght = 256;
+int symToInt(const char sym, const uint8_t textLenght, const uint8_t chunk, short int prewiosHash) {
+        char symbolsList[] = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ,.!?/:#$%&*+~`()-[]{}:;_'$<>";
+        short int i = 0; while (i != strlen(symbolsList) || i < hashLenght) {
+                if (sym == symbolsList[i]) {break;}
+                i++;
+        }
+        i = ((prewiosHash+chunk+17)*i*73*textLenght+prewiosHash)-(i*chunk*prewiosHash)-(993-textLenght-chunk);
+        if (i == 0) {i += textLenght;}
+        i = i % 10;
+        return std::abs(i);
+}
+std::string shash(const std::string text) {  // main
+        char hash[hashLenght];
+        uint8_t chunk = 0;
+        short int i = 0; int symInText = 0; while (i < hashLenght) {
+                if (sizeof(text) == symInText) {symInText = 0; chunk++; continue;}
+                else {
+                        hash[i] = symToInt(text[symInText], sizeof(text), chunk, hash[i-1]);}
+                i++;
+                symInText++;
+        }
+        i = 0; char finhash[hashLenght]; while (i < hashLenght) {
+                finhash[i] = '0' + hash[i];
+                i++;
+        }
+        std::string thash = finhash;
+        return thash;
+}
