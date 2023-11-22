e49711c3c0c99c838e179c32e36af2d925befa9c
xotnet <150250186+xotnet@users.noreply.github.com>
2023-11-22 15:54:04 +0000

    Update smartHash.hpp

--- smartHash.hpp
+++ smartHash.hpp
@@ -1,32 +1,37 @@
 #include <iostream>
 #include <cstring>
 #include <cstdint>
-const int hashLenght = 256;
-int symToInt(const char sym, const uint8_t textLenght, const uint8_t chunk, short int prewiosHash) {
+uint16_t hashLenght = 256;
+int symToInt(const char sym, const uint8_t textLenght, const uint8_t chunk, char hash[], const char prewiosHash, int u) {
         char symbolsList[] = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ,.!?/:#$%&*+~`()-[]{}:;_'$<>";
         short int i = 0; while (i != sizeof(symbolsList) || i < hashLenght) {
                 if (sym == symbolsList[i]) {break;}
                 i++;
         }
-        i = ((prewiosHash+chunk+17)*i*73*textLenght+prewiosHash)-(i*chunk*prewiosHash)-(993-textLenght-chunk);
-        if (i == 0) {i += textLenght;}
+        i = (((prewiosHash+chunk+17+textLenght)*i*73*textLenght+prewiosHash-1))-(i*chunk*prewiosHash)*(textLenght-chunk);
+        if (hash[u-1] + hash[u-2] == hash[u-3] + hash[u-4]) {
+			i = i * textLenght+chunk;
+		}
         i = i % 10;
         return std::abs(i);
 }
 std::string shash(const std::string text) {  // main
+		hashLenght++;
         char hash[hashLenght];
         uint8_t chunk = 0;
-        short int i = 0; int symInText = 0; while (i < hashLenght) {
-                if (sizeof(text) == symInText) {symInText = 0; chunk++; continue;}
+        short int i = 0; int symInText = 0; while (i != hashLenght) {
+                if (sizeof(text) == symInText) {symInText = 0; chunk += 3; continue;}
                 else {
-                        hash[i] = symToInt(text[symInText], sizeof(text), chunk, hash[i-1]);}
+					hash[i] = symToInt(text[symInText], sizeof(text), chunk, hash, hash[i-1], i);}
                 i++;
                 symInText++;
         }
-        i = 0; char finhash[hashLenght]; while (i < hashLenght-1) {
-                finhash[i] = '0' + hash[i];
+		i = 0; uint16_t u = 1;
+        char finhash[hashLenght]; while (i != hashLenght-1) {
+                finhash[i] = '0' + hash[hashLenght-u];
                 i++;
+				u++;
         }
 		std::string thash = finhash;
-        return thash;
+        return thash; 
 }
