05f35347f9cbdd101594ddcd0259af65a808a886
xotnet <150250186+xotnet@users.noreply.github.com>
2023-11-27 09:43:37 +0000

    Update smartHash.hpp

--- smartHash.hpp
+++ smartHash.hpp
@@ -2,36 +2,37 @@
 #include <cstring>
 #include <cstdint>
 uint16_t hashLenght = 256;
-int symToInt(const char sym, const uint8_t textLenght, const uint8_t chunk, const char prewiosHash, bool isRepeated, const char hash[], uint16_t u) {
+int symToInt(const char sym, const uint8_t textLenght, const uint8_t chunk, const char prewiosHash, bool isRepeated, uint16_t u) {
         char symbolsList[] = " abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ,.!?/:#$%&*+~`()-[]{}:;_'$<>";
         int64_t i = 0; while (i != sizeof(symbolsList) || i < hashLenght) {
 			if (sym == symbolsList[i]) {break;}
 			i++;
-        }
-		if (i%2==0) {i = i*24;} else {i = i*13;}
-		uint16_t prewSumOfHashChunk = 0;
-		if (hash[u-textLenght] != 0) {prewSumOfHashChunk = hash[u-textLenght] + hash[u-textLenght-1] + hash[u-textLenght-2] + hash[u-textLenght-3];}
-        i = (i+1)*73+chunk*(textLenght-prewiosHash-chunk-prewSumOfHashChunk) + ((prewSumOfHashChunk+prewiosHash+chunk)*2) + (i*chunk*prewiosHash*91) + chunk*4 - ((prewSumOfHashChunk-chunk-7)*prewiosHash) - (557/(prewiosHash+1+prewSumOfHashChunk));
-        if (isRepeated) {
-			i = 0-(i*(textLenght+prewiosHash+chunk)/7)*3;
-		}
+        } i = (i+1)*13+chunk*(textLenght-prewiosHash-chunk-i) + ((prewiosHash+chunk)*2) + (i*chunk*prewiosHash*11) + chunk*4 - ((chunk-7)*prewiosHash) - (557/(prewiosHash+1))*u;
+        if (isRepeated) {i = 0-(i*(textLenght+prewiosHash+chunk)/7)*u;}
+		while (i > 99999) {i = i/11;}
         i = i % 10;
         return std::abs(i);
 }
-std::string shash(const std::string text) {  //  MAIN
+std::string shash(const std::string text) {//     MAIN (Call it like: shash("Phase to hash"))
 		hashLenght++;
         char hash[hashLenght];
         uint16_t chunk = 0;
+		uint32_t skipSymbolsLength = 0;
+		if (text.length() > hashLenght) {
+			skipSymbolsLength = text.length()/hashLenght;
+			if (text.length() % 2 != 0) {
+				skipSymbolsLength = (text.length()-1)/hashLenght;
+			}
+		}
         uint16_t i = 0; int symInText = 0; while (i < hashLenght) {
 			if (text.length() == symInText) {symInText = 0; chunk++; continue;}
 			else {
 				bool isRepeated = false; if (hash[i-1] + hash[i-2] == hash[i-3] + hash[i-4]) {isRepeated = true;}
-				hash[i] = symToInt(text[symInText], text.length(), chunk, hash[i-1], isRepeated, hash, i);}
+				hash[i] = symToInt(text[symInText*skipSymbolsLength], text.length(), chunk, hash[i-1], isRepeated, i);}
 			i++;
 			symInText++;
         }
-		i = 0;
-        char finhash[hashLenght]; while (i < hashLenght-1) {
+		i = 0; char finhash[hashLenght]; while (i < hashLenght-1) {
                 finhash[i] = hash[hashLenght-1-i] + '0';
                 i++;
         }
